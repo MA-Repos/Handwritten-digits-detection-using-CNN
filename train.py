075879e8255a81d9a0038762d4ffb5054892c839
Mir Abbas <mir7abbas@gmail.com>
2019-08-04 16:46:06 -0400

    Model Creation

--- train.py
+++ train.py
@@ -1,5 +1,9 @@
+import keras
 from keras.datasets import mnist
 from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense, Conv2D, MaxPooling2D
+from keras.optimizers import SGD
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -12,7 +16,9 @@ print( len(yTrain), " -Labels in training dataset. " )
 print( len(xTest) , " -Sample dataset for testing. " )
 print( len(yTest) , " -Labels in test dataset. " )
 
+####################
 # Preprocess Data
+####################
 imgRows = xTrain[0].shape[0]
 imgCols = xTrain[0].shape[1]
 
@@ -34,6 +40,29 @@ print("xTrain shape: ", xTrain.shape)
 #labels encoding - yTrain & yTest
 yTrain = np_utils.to_categorical(yTrain)
 yTest = np_utils.to_categorical(yTest)
+numOfClasses = yTest.shape[1]
 
-print("No. of Classes: ", yTest.shape[1])
+print("No. of Classes: ", numOfClasses)
 
+
+#####################
+# Model Creation
+#####################
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=cnnInputShape))
+model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.3))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.6))
+model.add(Dense(numOfClasses, activation='softmax'))
+
+model.compile(loss= 'categorical_crossentropy', optimizer = SGD(0.1), metrics = ['accuracy'])
+
+print(model.summary())
+
+batchSize = 32
+epochs = 5
